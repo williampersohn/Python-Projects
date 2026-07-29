7edfc71015960357e9d3a2414d96159a7cbe428b
williampersohn <william.persohn@gmail.com>
2026-07-28 20:40:48 -0500

    Create cat.py

---
+++ cat.py
@@ -0,0 +1,18 @@
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+        return n
+
+
+def meow(n):
+    number = 0
+    #changed to fix problem ^
+    for _ in range(number):
+        print("meow")
